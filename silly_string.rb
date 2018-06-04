be9dad816430294ea8d6998d094c1f9f2abc1350
kileysel <kiley@selmanhouse.com>
2018-06-04 21:47:53 +0000

    Done.

--- silly_string.rb
+++ silly_string.rb
@@ -21,7 +21,7 @@ puts lowercased_phrase
 
 # Challenge 4: Count how many letters are in that big_word
 
-big_word = "supercalifragilisticexpialidocious"+ "wow" + 'mom'
+big_word = "supercalifragilisticexpialidocious"
 
 letter_count = big_word.length # Put a . after big_word and use a String method.
 
@@ -29,7 +29,7 @@ puts "There are #{letter_count} in #{big_word}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = big_word + "wow" + "mom" # Use String concatenation to add "wow" and "mom" to big_word
 
 new_letter_count = bigger_word.size
 
